eb54f60a8652cbd27c94d4353a76cc460f0f3585
hendratmoko <128827170+hendratmoko@users.noreply.github.com>
2026-05-16 05:14:35 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+
+  WEB_APP_URL:
+  "PASTE_WEB_APP_URL",
+
+  FONNTE_TOKEN:
+  "ISI_TOKEN_FONNTE",
+
+  ADMIN: {
+    username: "Miming",
+    password: "admin"
+  },
+
+  ADMIN_WA: "628121543986",
+  PENGADUAN_WA: "628813924881",
+
+  SCHOOL_NAME:
+  "SMK NEGERI 1 SANDEN"
+
+};
